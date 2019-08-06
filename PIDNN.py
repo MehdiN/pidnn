a19e2f62c7e64c71206f889c3f5ce1190d5210af
mehdin <nima.mehdi@ensta-bretagne.org>
2019-08-06 22:46:30 +0200

    fixed class issues

--- PIDNN.py
+++ PIDNN.py
@@ -1,7 +1,3 @@
-import numpy as np
-from numpy import array
-
-
 class PIDNN(object):
     '''A PID Neural Network Controller Class
 
@@ -22,24 +18,25 @@ class PIDNN(object):
         self.W2 = np.zeros((self.dim_state, 1))
         self.I = {}
         self.D = {}
+        self.cache = {}
 
     def init_net(self):
         """
         Initialization of the Neural Network
 
         ...
-        We randomly initialize the ceights
+        We randomly initialize the weights
 
         """
         self.W2 = np.random.randn(self.dim_state, 3 * self.dim_state) * 0.01
         for i in range(self.dim_state):
-            self.W1["w" + str(i)] = np.random.randn(3, 2) * 0.01
+            self.W1["W1" + str(i)] = np.random.randn(3, 2) * 0.01
             self.I['I' + str(i)] = 0
             self.D["D" + str(i)] = 0
 
-    # activation functions
+    # Activation functions
 
-    def p_neuron(v):
+    def p_neuron(self, v):
         """
         P Neuron Activation Function
 
@@ -50,7 +47,7 @@ class PIDNN(object):
             v = v / np.fabs(v)
         return v
 
-    def i_neuron(v, I):
+    def i_neuron(self, v, I):
         """
         I Neuron Activation Function
 
@@ -63,20 +60,20 @@ class PIDNN(object):
             v = v + I
         return v
 
-    def d_neuron(v, D):
+    def d_neuron(self, v, D):
         """
         D Neuron Activation Function
 
         ...
         Return input - derivative term for input between -1 and 1
         """
-        if np.fabs(x) > 1:
+        if np.fabs(v) > 1:
             v = v / np.fasb(v)
         else:
-            x = v - D
+            v = v - D
         return v
 
-    def neuron_back(z):
+    def neuron_back(self, z):
         """
         Derivative function for backpropagation computation
 
@@ -107,18 +104,19 @@ class PIDNN(object):
             D = self.D["D" + str(i)]
             X = np.concatenate((x[i], r[i]))
             X = X.reshape((2, -1))
-            W1 = W1['W1' + str(i)]
-            Z1 = np.dot(W1, X)
+            W = self.W1['W1' + str(i)]
+            Z1 = np.dot(W, X)
             Z1_cache = np.append(Z1_cache, Z1)
-            a1[0] = p_neuron(Z[0].squeeze())
-            a1[1] = i_neuron(Z[1].squeeze(), I)
-            a1[2] = d_neuron(Z[2].squeeze(), D)
+            a1[0] = self.p_neuron(Z1[0][0])
+            a1[1] = self.i_neuron(Z1[1][0], I)
+            a1[2] = self.d_neuron(Z1[2][0], D)
             A1["a1" + str(i)] = a1
             # update pid integrator and derivator
             self.I["I" + str(i)] = a1[1].squeeze()
             self.D["D" + str(i)] = a1[2].squeeze()
         Z1_cache = Z1_cache.reshape((Z1_cache.shape[0], -1))
-        return A1, Z1_cache
+        self.cache["Z1"] = Z1_cache
+        return A1
 
     def forward_output(self, A1):
         """
@@ -136,14 +134,15 @@ class PIDNN(object):
         _A1 = []
         for i in range(self.dim_state):
             a1 = A1['a1' + str(i)]
-            _A1 = np.append((_A1, a1))
-        _A1 = _A1.reshape((dim_state * 3, -1))
-        Z2 = np.dot(W2, _A1)
+            _A1 = np.append(_A1, a1)
+        _A1 = _A1.reshape((self.dim_state * 3, -1))
+        self.cache['A1'] = _A1
+        Z2 = np.dot(self.W2, _A1)
         A2 = np.zeros(Z2.shape)
         # saturate the output;
-        for i in (len(A2)):
-            A2[i] = p_neuron(A2[i])
-        return A2
+        for i in range(len(A2)):
+            A2[i][0] = self.p_neuron(A2[i][0])
+        self.cache["A2"] = A2
 
     def compute_cost(self, r, y):
         """
@@ -153,7 +152,7 @@ class PIDNN(object):
         loss = 0.5 * np.dot((r - y).T, r - y)
         return loss
 
-    def backward_pass_1(self, A2, Y):
+    def backward_pass_1(self, Y):
         """
         First backward pass function
 
@@ -168,11 +167,14 @@ class PIDNN(object):
         dZ2: output gradient
         """
         # compute dW2
+        A2 = self.cache["A2"]
         dZ2 = A2 - Y
+        self.cache["dZ2"] = dZ2
+        A1 = self.cache['A1']
         dW2 = np.dot(dZ2, A1.T)
-        return dW2, dZ2
+        return dW2
 
-    def backward_pass_2(self, dZ2, Z1_cache, x, y):
+    def backward_pass_2(self, x, y):
         """
         Second backward pass function
 
@@ -187,16 +189,17 @@ class PIDNN(object):
         dW1: weight gradient
         """
         # compute dW1
-        dZ1 = np.dot(W2.T, dZ2) * neuron_back(Z1_cache)
-        dZ1 = np.dot(W2, dZ2) * self.neuron_back(Z1)
+        dZ2 = self.cache["dZ2"]
+        Z1 = self.cache["Z1"]
+        dZ1 = np.dot(self.W2.T, dZ2) * self.neuron_back(Z1)
         dW1 = {}
-        for i in range(dim_state):
+        for i in range(self.dim_state):
             X = array([x[i], r[i]])
             X = X.reshape((2, -1))
-            dW1["dW1" + str(i)] = np.dot(dZ1[i:i + 3], X_cache[i:i + 2].T)
+            dW1["dW1" + str(i)] = np.dot(dZ1[i:i + 3], X.T)
         return dW1
 
-    def update_weigths(self, W1, W2, dW1, dW2):
+    def update_weights(self, dW1, dW2):
         """
         Gradient descent update step
 
@@ -206,19 +209,21 @@ class PIDNN(object):
         W1,W1: weight
         dW1,dW2: gradient weight
         """
-        W2 += -1.0 * learning_rate * grad2
+        self.W2 += -1.0 * learning_rate * dW2
         for i in range(self.dim_state):
-            self.W1["W1" + str(i)] += -1.0 * learning_rate * \
-                grad1["dW1" + str(i)]
+            self.W1["W1" + str(i)] += -1.0 * \
+                learning_rate * dW1["dW1" + str(i)]
 
     def feedforward(self, x, y):
         # FeedFoward
-        A1, Z1_cache = self.pid_forward_pass(x, y)
-        A2 = self.forward_output(A1)
+        A1 = self.pid_forward_pass(x, y)
+        self.forward_output(A1)
+        u = self.cache["A2"]
+        return u
 
-    def backprop(self, x, r, A2, Y):
+    def backprop(self, x, r, Y):
         # Backprop
-        dW2, dZ2 = self.backward_pass_1(A2, Y)
-        dW1 = self.backward_pass_2(dZ2, Z1_cache, x, y)
+        dW2 = self.backward_pass_1(Y)
+        dW1 = self.backward_pass_2(x, y)
         # Gradient Descent
-        self.update_weigths(W1, W2, dW1, dW2)
+        self.update_weights(dW1, dW2)
